4588dd1be5f0b755d80d7224e86fb3a062fc6030
Divya <divyamohanan@gmail.com>
2018-10-12 22:58:14 -0700

    Works index views changed

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,17 @@
 class WorksController < ApplicationController
+  def index
+    @works = Work.all
+    @movies = []
+    @books = []
+    @albums = []
+    @works.each do |work|
+      if work.category == "movie"
+        @movies << work
+      elsif work.category == "book"
+        @books << work
+      else
+        @albums << work
+      end
+    end
+  end
 end
