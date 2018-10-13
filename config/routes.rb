4bb8511c7a586167105c80c3791533aa1b240b93
Divya <divyamohanan@gmail.com>
2018-10-12 20:21:50 -0700

    Created new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'works#top'
+
+  resources :works
+
+  resources :users
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  post 'works/:id/upvote', to: 'votes#upvote', as: 'new_vote'
 end
